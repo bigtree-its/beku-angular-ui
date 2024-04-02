f84e2acad394ac3d3eb0a847b2673e4d35a73500
bigtree_it_s <bigtree.it.services@gmail.com>
2024-04-02 17:32:07 +0100

    Module Foods

---
+++ src/app/foods/pages/index.ts
@@ -0,0 +1,5 @@
+export * from './cart/cart.component';
+export * from './checkout/checkout.component';
+export * from './product-list/product-list.component';
+export * from './group-list/group-list.component';
+export * from './product-detail/product-detail.component';
